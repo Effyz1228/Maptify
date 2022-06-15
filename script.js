d240bfda1777e0280464aa9dfc933e6d9b1be065
Effy Zhang <effyz1228@gmail.com>
2022-06-15 14:52:31 -0700

    Codes before refactoring

---
+++ script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvt) {
+        // console.log(mapEvt);
+        mapEvent = mapEvt;
+        form.classList.remove('hidden');
+      });
+    },
+    function () {
+      alert("Can't access your position!");
+    }
+  );
+}
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        className: 'running-popup',
+        closeOnClick: false,
+      })
+    )
+    .setPopupContent('worked out here!')
+    .openPopup();
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+});
+
+inputType.addEventListener('change', () => {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
